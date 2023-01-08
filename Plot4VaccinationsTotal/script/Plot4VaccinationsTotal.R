1a6af49fc9a5d7e4c44731ad22924d5e98d7b477
Jack <jack37feus@hotmail.com>
2023-01-08 20:26:44 +0200

    smoothed data + added dose legend

--- Plot4VaccinationsTotal/script/Plot4VaccinationsTotal.R
+++ Plot4VaccinationsTotal/script/Plot4VaccinationsTotal.R
@@ -3,10 +3,12 @@ rm(list=ls())
 
 
 #install packages
+
 library(tidyverse)
 library(gganimate)
 library(gifski)
 library(scales)
+library(RcppRoll)
 
 #load data 
 
@@ -73,28 +75,34 @@ newdoses <- raw %>%
     age%in%c("50_54", "55_59")~"50-59",
     age%in%c("60_64", "65_69")~"60-69",
     age%in%c("70_74", "75_79")~"70-79",
-    age%in%c("80_84", "85_89", "90+")~"80+"
-  ),
+    age%in%c("80_84", "85_89", "90+")~"80+"),
   date=as.Date(date))%>%
-  group_by(date, age)%>%
+  group_by(age, date)%>%
+  
+#add the rolling sum
   summarize(pop=sum(pop),
             firstdose=sum(firstdose),
             seconddose=sum(seconddose),
             thirddose=sum(thirddose))%>%
   ungroup()%>%
+  group_by(age)%>%
+  arrange(date)%>%
+  mutate(firstdoseroll = roll_sum(firstdose, n=7, fill=NA, align="left")/7,
+         seconddoseroll = roll_sum(seconddose, n=7, fill=NA, align="left")/7,
+         thirddoseroll = roll_sum(thirddose, n=7, fill=NA, align="left")/7)%>%
   filter(date<="2022-02-01")
 
 plot2 <- ggplot(newdoses)+
-  geom_line(aes(x=date,y=firstdose, colour=age))+
-  geom_line(aes(x=date,y=seconddose, colour=age), linetype="dashed")+
-  geom_line(aes(x=date,y=thirddose, colour=age), linetype="dotted")+
+  geom_line(aes(x=date,y=firstdoseroll, colour=age))+
+  geom_line(aes(x=date,y=seconddoseroll, colour=age), linetype="dashed")+
+  geom_line(aes(x=date,y=thirddoseroll, colour=age), linetype="dotted")+
   labs(x="",
        y="",
        title = "New COVID-19 vaccine doses administered by dose type - UK",
        subtitle="First dose (solid), second dose (dashed), third dose (dotted)",
        caption="Data from coronavirus.data.gov\nProduced by @JackD1801",
        colour="")+ 
-  scale_x_date()+
+  scale_x_date(date_labels = "%b-%y")+
   scale_y_continuous(expand=c(0,0), labels = label_comma())+
   theme_classic()+
   theme(legend.position="bottom")+
@@ -111,3 +119,27 @@ plot2.animation
 animate(plot2.animation, height=500, width=800, fps=30, duration = 25, 
         end_pause=60, res=100)
 
+#tidy data version
+
+tidydoses <- newdoses %>%
+              select(date, age, firstdoseroll, seconddoseroll, thirddoseroll)%>%
+              rename(first = firstdoseroll,
+                     second = seconddoseroll,
+                     third = thirddoseroll)%>%
+              pivot_longer(cols=c("First", "Second", "Third"), names_to="dose", values_to="total")
+
+plot3 <- ggplot(tidydoses)+
+  geom_line(aes(x=date,y=total, colour=age, linetype=dose))+
+  labs(x="",
+       y="",
+       title = "New COVID-19 vaccine doses administered by dose type - UK",
+       subtitle="",
+       caption="Data from coronavirus.data.gov\nProduced by @JackD1801",
+       colour="")+ 
+  scale_x_date(date_labels = "%b-%y")+
+  scale_y_continuous(expand=c(0,0), labels = label_comma())+
+  theme_classic()+
+  theme(legend.position="bottom", legend.box="vertical", legend.margin=margin())+
+  guides(colour=guide_legend(nrow=1))
+#geom_text(data=newdoses, aes(label=age, x=date+6, y=firstdose, colour=age), size=4)
+plot3
